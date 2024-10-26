febbf6129925eda4318cb0dff38550d93fecb7f4
Катя <@DESKTOP-8CVJE59>
2024-10-26 22:47:25 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть довжину масиву: ");
+        int length = int.Parse(Console.ReadLine());
+        int[] array = new int[length];
+        Random rand = new Random();
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rand.Next(-5, 11);
+        }
+        Console.WriteLine("Вихідний масив:");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine($"array[{i}] = {array[i]}");
+        }
+        Console.WriteLine("Локальні максимуми:");
+        for (int i = 1; i < array.Length - 1; i++)
+        {
+            if (array[i] > array[i - 1] && array[i] > array[i + 1])
+            {
+                Console.WriteLine($"Локальний максимум: array[{i}] = {array[i]}");
+            }
+        }
+    }
+}
